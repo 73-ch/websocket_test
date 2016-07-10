82dd38cc7af1f53d385894bd22aed935ccea7fbc
nami634 <cigarbox.nami@gmail.com>
2016-07-10 09:16:09 +0900

    changed tests#send

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -6,11 +6,6 @@ class TestsController < WebsocketRails::BaseController
   def send
   	channel = message[:chn]
   	WebsocketRails["#{channel}"].trigger(:send, message)
-  	if message[:chn]
-	    trigger_success {message => 'awesome level is sufficient'}
-	  else
-	    trigger_failure {message => 'awesome level is insufficient'}
-	  end
   end
 
 end
